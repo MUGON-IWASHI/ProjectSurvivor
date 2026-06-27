cc11fa98e62cc590e00f4ef7fb0db2716d3d85ef
shuto0211-admin <suuuuutooooo@gmail.com>
2026-06-27 21:46:45 +0900

    Implement C++ PlayerCharacter with Enhanced Input

--- Source/ProjectSurvivor/Character/PlayerCharacter.cpp
+++ Source/ProjectSurvivor/Character/PlayerCharacter.cpp
@@ -57,3 +57,29 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 }
 
+void APlayerCharacter::Move(const FInputActionValue& Value) {
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr) {
+
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value) {
+
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+	if (Controller != nullptr) {
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
